6d4a6ec90a923b1047d3d6f8a73d0e64126e1251
PunkChou <79570071+mtldxwz@users.noreply.github.com>
2026-04-04 18:22:21 +0800

    feat: add UserRepository

--- src/main/java/com/lobsterbox/repository/UserRepository.java
+++ src/main/java/com/lobsterbox/repository/UserRepository.java
@@ -2,8 +2,14 @@ package com.lobsterbox.repository;
 
 import com.lobsterbox.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByOpenid(String openid);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByAgentId(String agentId);
+    boolean existsByEmail(String email);
+    boolean existsByAgentId(String agentId);
 }
